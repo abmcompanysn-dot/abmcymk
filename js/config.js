babe6aa093156d6c308da25e4cbb9a2a6990ff06
president <abmcompanysn@gmail.com>
2025-10-07 19:36:49 +0000

    Mise à jour du produit ou de l’interface

--- js/config.js
+++ js/config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // URL de l'API publique (Script 2: Gestion Client & Livraison)
     // À REMPLIR APRÈS LE DÉPLOIEMENT
-    CLIENT_API_URL: "https://script.google.com/macros/s/AKfycbykJzbujkpNHKmBOTXLsftzqfEjWYQBsa7Z2iCGQGvJ66XMRcSbH5hzqxiPKcu9HhDT/exec",
+    CLIENT_API_URL: "https://script.google.com/macros/s/AKfycbw8P9FEvCqw9xXDkvK7BY_RQ-wTZSKoIuxV1tVHj1RGE9rUSMMyaqqQGQcfpi9_Rsax/exec",
 
     // URL de l'API d'administration (Script 1: Gestion Produits & Front-End)
     // À REMPLIR APRÈS LE DÉPLOIEMENT
-    ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbwB5wmfFB-PSfMl7UUFKM2W1IAOf7W9hiurgfrPYMouDeZlWDu10lz2DFV074f-5Ksy/exec",
+    ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbzYGOU2yHC1tfQmRo9Eh3x2_K8dKe_jyhj30aprb_Q3MO3ljilTilS8QqMmBsn95AFp/exec",
 
     // Autres configurations
     DEFAULT_PRODUCT_IMAGE: "https://i.postimg.cc/6QZBH1JJ/Sleek-Wordmark-Logo-for-ABMCY-MARKET.png",
