b4c47fdc40e175c73741004864a03228c6de4911
president <abmcompanysn@gmail.com>
2025-10-11 22:48:58 +0000

    Mise à jour du produit ou de l’interface

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // URL de l'API publique (Script 2: Gestion Client & Livraison)
-    CLIENT_API_URL: "https://script.google.com/macros/s/AKfycbx_MpBD0lrp4Px2-tRcbT2Fjxptz9-7b8UPjc7G278steM3H27Ez0nQ0vMIHsPyq-cL/exec",
+    CLIENT_API_URL: "https://script.google.com/macros/s/AKfycbxxPxwxm1jgg7tizV43J5Gy-p_TFZP3F1RyI6c3jlpXkYp4hbL9LTtlTfnrxmcK7bv9/exec",
 
     // URL du script central. On ajoute l'action dans la requête fetch.
     CENTRAL_API_URL: "https://script.google.com/macros/s/AKfycbwYJ20BjaSTD1MjOAJbGXbmPKZGdbrVgp4j6w0eg8dVEMmPfpxkoTyvT69rlbe7Fx8R/exec",
