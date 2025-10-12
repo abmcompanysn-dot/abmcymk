15a50aef585cb425c35cfdfa161994129caef774
president <abmcompanysn@gmail.com>
2025-10-12 17:33:01 +0000

    Mise à jour du produit ou de l’interface

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // URL de l'API pour la gestion des comptes (authentification, etc.)
-    ACCOUNT_API_URL: "https://script.google.com/macros/s/AKfycbwRpCI4watBHo8R6jkvcVpPb8pTVTbtR5Kg2vOvHby1Kdweo7nx4G-LEMfE9HQ5vRnc/exec",
+    ACCOUNT_API_URL: "https://script.google.com/macros/s/AKfycbwQ0B7PFpGhes-Bjf1Fv2FfvoAZ2sohEjOTG1V52MFybUl4JkZxMnO_EJupQA_sgmUh/exec",
     // NOUVEAU: URL de l'API dédiée à la gestion des commandes
     ORDER_API_URL: "https://script.google.com/macros/s/AKfycbwrLOxtez1UrrXVj2iugSiLtAYOhhaef9x-HlpvvvxmQxsMVYRVQeShy4V56vfhjBxq/exec",
 
